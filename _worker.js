f34da5614e4930702d9a09d626e034e552b8156e
wardehuang <wardehuang@users.noreply.github.com>
2026-05-19 11:32:52 +0800

    还原 subConfig 指向本仓库配置

--- _worker.js
+++ _worker.js
@@ -16,7 +16,7 @@ let MainData = ``;
 
 let urls = [];
 let subConverter = "SUBAPI.cmliussss.net"; //在线订阅转换后端，目前使用CM的订阅转换功能。支持自建psub 可自行搭建https://github.com/bulianglin/psub
-let subConfig = "https://raw.githubusercontent.com/cmliu/ACL4SSR/main/Clash/config/ACL4SSR_Online_MultiCountry.ini"; //订阅配置文件
+let subConfig = "https://raw.githubusercontent.com/wardehuang/CF-Workers-SUB/main/config/ACL4SSR_Online_MultiCountry.ini"; //订阅配置文件
 let subProtocol = 'https';
 
 export default {
